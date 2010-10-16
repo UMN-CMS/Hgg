ee14507a4d6350331b3bfe9efe1ea4c6eadfc4e4
franzoni <>
2010-10-16 07:36:00 +0000

    removing pi0 from the name of the TTeee sought after

--- ECALTime/EcalTimePi0/bin/readEcalTimePi0Tree.cpp
+++ ECALTime/EcalTimePi0/bin/readEcalTimePi0Tree.cpp
@@ -2602,7 +2602,7 @@ int main (int argc, char** argv)
   }
 
   // Tree construction
-  TChain * chain = new TChain ("EcalTimePi0Analysis") ;
+  TChain * chain = new TChain ("EcalTimeAnalysis") ;
   std::vector<std::string>::const_iterator file_itr;
   for(file_itr=listOfFiles_.begin(); file_itr!=listOfFiles_.end(); file_itr++){
     chain->Add( (*file_itr).c_str() );
