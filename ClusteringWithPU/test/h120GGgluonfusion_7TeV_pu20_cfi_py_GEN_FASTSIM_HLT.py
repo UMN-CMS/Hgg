c4429b71c27b2664cd2c0ec7c17db515daf71a14
franzoni <>
2011-03-23 16:50:04 +0000

    importing  fast sim for Hgg + RECO + usage of analysis

---
+++ ClusteringWithPU/test/h120GGgluonfusion_7TeV_pu20_cfi_py_GEN_FASTSIM_HLT.py
@@ -0,0 +1,180 @@
+# Auto generated configuration file
+# using: 
+# Revision: 1.284.2.2 
+# Source: /cvs_server/repositories/CMSSW/CMSSW/Configuration/PyReleaseValidation/python/ConfigBuilder.py,v 
+# with command line options: H130GGgluonfusion_7TeV_cfi.py -s GEN,FASTSIM,HLT:GRun --pileup=NoPileUp --geometry DB --conditions=auto:mc --eventcontent=RECOSIM --datatier GEN-SIM-DIGI-RECO -n 10 --no_exec
+import FWCore.ParameterSet.Config as cms
+
+process = cms.Process('HLT')
+
+
+# import of standard configurations
+process.load('Configuration.StandardSequences.Services_cff')
+process.load('SimGeneral.HepPDTESSource.pythiapdt_cfi')
+
+process.load('FWCore.MessageService.MessageLogger_cfi')
+process.MessageLogger.cerr.FwkReport.reportEvery = 10
+
+process.load('FastSimulation.PileUpProducer.PileUpSimulator_NoPileUp_cff')
+process.load('FastSimulation.Configuration.Geometries_MC_cff')
+process.load('Configuration.StandardSequences.MagneticField_38T_cff')
+process.load('Configuration.StandardSequences.Generator_cff')
+process.load('GeneratorInterface.Core.genFilterSummary_cff')
+process.load('FastSimulation.Configuration.FamosSequences_cff')
+process.load('IOMC.EventVertexGenerators.VtxSmearedParameters_cfi')
+process.load('FastSimulation.Configuration.HLT_GRun_cff')
+process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
+process.load('FastSimulation.Configuration.EventContent_cff')
+
+process.maxEvents = cms.untracked.PSet(
+    input = cms.untracked.int32(10)
+)
+
+#https://twiki.cern.ch/twiki/bin/view/CMSPublic/SWGuideEDMRandomNumberGeneratorService
+#For example, CRAB will by default set the seeds in the different processes to random numbers
+
+# Input source
+process.source = cms.Source("EmptySource")
+
+process.options = cms.untracked.PSet(
+
+)
+
+# Production Info
+process.configurationMetadata = cms.untracked.PSet(
+    version = cms.untracked.string('$Revision: 1.284.2.2 $'),
+    annotation = cms.untracked.string('H120GGgluonfusion_7TeV_cfi.py nevts:10'),
+    name = cms.untracked.string('PyReleaseValidation')
+)
+
+# Output definition
+
+process.RECOSIMoutput = cms.OutputModule("PoolOutputModule",
+    splitLevel = cms.untracked.int32(0),
+    outputCommands = process.RECOSIMEventContent.outputCommands,
+    fileName = cms.untracked.string('test-H120GGgluonfusion_7TeV_cfi_py_GEN_FASTSIM_HLT.root'),
+    dataset = cms.untracked.PSet(
+        filterName = cms.untracked.string(''),
+        dataTier = cms.untracked.string('GEN-SIM-DIGI-RECO')
+    ),
+    SelectEvents = cms.untracked.PSet(
+        SelectEvents = cms.vstring('generation_step')
+    )
+)
+
+process.TFileService = cms.Service("TFileService",
+    fileName = cms.string('gf2_SCwithPUAnalysis_Hgg_PU20_testCoffee.root')
+)
+
+# Additional output definition
+
+# Other statements
+process.famosSimHits.SimulateCalorimetry = True
+process.famosSimHits.SimulateTracking = True
+process.simulation = cms.Sequence(process.simulationWithFamos)
+process.HLTEndSequence = cms.Sequence(process.reconstructionWithFamos)
+process.Realistic7TeVCollisionVtxSmearingParameters.type = cms.string("BetaFunc")
+process.famosSimHits.VertexGenerator = process.Realistic7TeVCollisionVtxSmearingParameters
+process.famosPileUp.VertexGenerator = process.Realistic7TeVCollisionVtxSmearingParameters
+process.GlobalTag.globaltag = 'MC_311_V2::All'
+
+
+process.famosPileUp.PileUpSimulator.averageNumber = 20
+
+
+process.generator = cms.EDFilter("Pythia6GeneratorFilter",
+    pythiaPylistVerbosity = cms.untracked.int32(1),
+    filterEfficiency = cms.untracked.double(1.0),
+    pythiaHepMCVerbosity = cms.untracked.bool(False),
+    comEnergy = cms.double(7000.0),
+    crossSection = cms.untracked.double(0.05),
+    maxEventsToPrint = cms.untracked.int32(3),
+    PythiaParameters = cms.PSet(
+        pythiaUESettings = cms.vstring('MSTJ(11)=3     ! Choice of the fragmentation function', 
+            'MSTJ(22)=2     ! Decay those unstable particles', 
+            'PARJ(71)=10 .  ! for which ctau  10 mm', 
+            'MSTP(2)=1      ! which order running alphaS', 
+            'MSTP(33)=0     ! no K factors in hard cross sections', 
+            'MSTP(51)=10042 ! structure function chosen (external PDF CTEQ6L1)', 
+            'MSTP(52)=2     ! work with LHAPDF', 
+            'MSTP(81)=1     ! multiple parton interactions 1 is Pythia default', 
+            'MSTP(82)=4     ! Defines the multi-parton model', 
+            'MSTU(21)=1     ! Check on possible errors during program execution', 
+            'PARP(82)=1.8387   ! pt cutoff for multiparton interactions', 
+            'PARP(89)=1960. ! sqrts for which PARP82 is set', 
+            'PARP(83)=0.5   ! Multiple interactions: matter distrbn parameter', 
+            'PARP(84)=0.4   ! Multiple interactions: matter distribution parameter', 
+            'PARP(90)=0.16  ! Multiple interactions: rescaling power', 
+            'PARP(67)=2.5    ! amount of initial-state radiation', 
+            'PARP(85)=1.0  ! gluon prod. mechanism in MI', 
+            'PARP(86)=1.0  ! gluon prod. mechanism in MI', 
+            'PARP(62)=1.25   ! ', 
+            'PARP(64)=0.2    ! ', 
+            'MSTP(91)=1      !', 
+            'PARP(91)=2.1   ! kt distribution', 
+            'PARP(93)=15.0  ! '),
+        processParameters = cms.vstring('PMAS(25,1)=120.0      !mass of Higgs', 
+            'MSEL=0                  ! user selection for process', 
+            'MSUB(102)=1             !ggH', 
+            'MSUB(123)=0             !ZZ fusion to H', 
+            'MSUB(124)=0             !WW fusion to H', 
+            'MSUB(24)=0              !ZH production', 
+            'MSUB(26)=0              !WH production', 
+            'MSUB(121)=0             !gg to ttH', 
+            'MSUB(122)=0             !qq to ttH', 
+            'MDME(210,1)=0           !Higgs decay into dd', 
+            'MDME(211,1)=0           !Higgs decay into uu', 
+            'MDME(212,1)=0           !Higgs decay into ss', 
+            'MDME(213,1)=0           !Higgs decay into cc', 
+            'MDME(214,1)=0           !Higgs decay into bb', 
+            'MDME(215,1)=0           !Higgs decay into tt', 
+            'MDME(216,1)=0           !Higgs decay into', 
+            'MDME(217,1)=0           !Higgs decay into Higgs decay', 
+            'MDME(218,1)=0           !Higgs decay into e nu e', 
+            'MDME(219,1)=0           !Higgs decay into mu nu mu', 
+            'MDME(220,1)=0           !Higgs decay into tau nu tau', 
+            'MDME(221,1)=0           !Higgs decay into Higgs decay', 
+            'MDME(222,1)=0           !Higgs decay into g g', 
+            'MDME(223,1)=1           !Higgs decay into gam gam', 
+            'MDME(224,1)=0           !Higgs decay into gam Z', 
+            'MDME(225,1)=0           !Higgs decay into Z Z', 
+            'MDME(226,1)=0           !Higgs decay into W W'),
+        parameterSets = cms.vstring('pythiaUESettings', 
+            'processParameters')
+    )
+)
+
+
+# Path and EndPath definitions
+process.generation_step = cms.Path(process.pgen_genonly)
+process.reconstruction = cms.Path(process.reconstructionWithFamos)
+process.genfiltersummary_step = cms.EndPath(process.genFilterSummary)
+process.RECOSIMoutput_step = cms.EndPath(process.RECOSIMoutput)
+
+process.scwithpuanalyzer = cms.EDAnalyzer('SCwithPUAnalysis')
+process.analysisPath = cms.Path(process.scwithpuanalyzer)
+
+# Schedule definition
+process.schedule = cms.Schedule(process.generation_step,process.genfiltersummary_step)
+process.schedule.extend(process.HLTSchedule)
+process.schedule.extend([
+	process.reconstruction,
+	# process.RECOSIMoutput_step,
+	process.analysisPath
+	])
+# filter all path with the production filter sequence
+for path in process.paths:
+	getattr(process,path)._seq = process.generator * getattr(process,path)._seq 
+
+
+#process.options = cms.untracked.PSet(
+#	     wantSummary = cms.untracked.bool(True)
+#      )
+
+## get the time summary at the end of the process
+#process.TimerService = cms.Service("TimerService")
+#process.options = cms.untracked.PSet(
+#	useCPUtime = cms.untracked.bool(True),
+#        wantSummary = cms.untracked.bool(True)
+#        )
+#
