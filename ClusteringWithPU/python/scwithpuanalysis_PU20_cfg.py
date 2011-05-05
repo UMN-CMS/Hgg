850598dd5f6c75b95c1d964672d1c4962cb4f906
gude <>
2011-05-05 10:02:44 +0000

    This commit was manufactured by cvs2svn to create tag 'V00-01-00'.
    Sprout from master 2009-02-17 19:53:41 UTC haupt 'Needed updates for the 2D binned efficiencies compatability part of the code'
    Cherrypick from master 2010-07-05 14:23:00 UTC scooper 'Adding L1 triggers and L1 technical triggers to the tree.':
        ECALTime/EcalTimePi0/BuildFile
        ECALTime/EcalTimePi0/bin/BuildFile
        ECALTime/EcalTimePi0/bin/readEcalTimePi0Tree.cpp
        ECALTime/EcalTimePi0/interface/EcalTimePi0TreeContent.h
        ECALTime/EcalTimePi0/plugins/BuildFile
        ECALTime/EcalTimePi0/plugins/EcalTimePi0Tree.cc
        ECALTime/EcalTimePi0/plugins/EcalTimePi0Tree.h
        ECALTime/EcalTimePi0/plugins/plugins.cc
        ECALTime/EcalTimePi0/python/ecalTimePi0MakeTreeFromRaw_cfg.py
        ECALTime/EcalTimePi0/python/ecalTimePi0MakeTree_cfg.py
        ECALTime/EcalTimePi0/python/ecalTimePi0Tree_cfi.py
        ECALTime/EcalTimePi0/src/EcalTimePi0TreeContent.cc
        ZShape/Base/interface/ZShapeEvent.h
        ZShape/Base/interface/ZShapeStandardCuts.h
        ZShape/Base/src/ZShapeEvent.cc
        ZShape/Base/src/ZShapeStandardCuts.cc
        ZShape/Base/src/ZShapeZDef.cc
    Cherrypick from master 2011-05-05 10:02:43 UTC franzoni 'moved here from src, after refactorization':
        Hgg/ClusteringWithPU/BuildFile.xml
        Hgg/ClusteringWithPU/interface/SCwithPUhistos.h
        Hgg/ClusteringWithPU/interface/Utils.h
        Hgg/ClusteringWithPU/macro/ClusteringWithPU_macro.C
        Hgg/ClusteringWithPU/plugins/BuildFile.xml
        Hgg/ClusteringWithPU/plugins/SCwithPUAnalysis.cc
        Hgg/ClusteringWithPU/python/scwithpuanalysis_PU20_cfg.py
        Hgg/ClusteringWithPU/src/SCwithPUhistos.cc
        Hgg/ClusteringWithPU/src/Utils.cc
        Hgg/ClusteringWithPU/test/h120GGgluonfusion_7TeV_pu20_cfi_py_GEN_FASTSIM_HLT.py
    Cherrypick from master 2009-06-08 19:50:56 UTC haupt 'Allowing a number of Gsf electron requirement':
        ZShape/Base/interface/ZShapeZDef.h
        ZShape/Base/src/EtaAcceptance.cc
    Delete:
        ZShape/EffAcc/tool/photosLHEF/HEPEVT.h
        ZShape/EffAcc/tool/photosLHEF/Makefile
        ZShape/EffAcc/tool/photosLHEF/PHOTOSintf.h
        ZShape/EffAcc/tool/photosLHEF/photos-random.h
        ZShape/EffAcc/tool/photosLHEF/photos.F
        ZShape/EffAcc/tool/photosLHEF/photosLHEF.cc

---
+++ ClusteringWithPU/python/scwithpuanalysis_PU20_cfg.py
@@ -0,0 +1,34 @@
+import FWCore.ParameterSet.Config as cms
+
+process = cms.Process("SCwithPUAnalyzer")
+
+process.load("FWCore.MessageService.MessageLogger_cfi")
+process.MessageLogger.cerr.FwkReport.reportEvery = 1000
+
+process.TimerService = cms.Service("TimerService")
+process.options = cms.untracked.PSet(
+        wantSummary = cms.untracked.bool(True)
+        )
+
+
+process.TFileService = cms.Service("TFileService",
+    fileName = cms.string('SCwithPUAnalysis_Hgg_PU20_v2.root')
+)
+
+process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(500) )
+
+process.source = cms.Source("PoolSource",
+                            duplicateCheckMode = cms.untracked.string("noDuplicateCheck"),
+                            fileNames = cms.untracked.vstring(
+#        'file:/data2/kubota/CMSSW_3_9_4/src/H120GGgluonfusion_PU20_1.root',
+        'file:/data/franzoni/data/superClusterPU/H120GGgluonfusion_PU20_9.root'
+#        'rfio:/castor/cern.ch/user/f/futyand/SCwithPU/H120GGgluonfusion_PU20_1.root'
+#        'rfio:/castor/cern.ch/user/f/futyand/SCwithPU/H120GGgluonfusion_PU20_2.root',
+    )
+)
+
+process.scwithpuanalyzer = cms.EDAnalyzer('SCwithPUAnalysis'
+)
+
+
+process.p = cms.Path(process.scwithpuanalyzer)
