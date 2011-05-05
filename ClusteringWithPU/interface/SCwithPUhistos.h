f8ca72b0d509ec8edf56f7c22ab48a1cb8a076a6
gude <>
2011-05-05 19:06:38 +0000

    This commit was manufactured by cvs2svn to create tag 'V00-01-01'.
    Sprout from master 2009-04-16 20:03:44 UTC klapoet 'Tuned smearing parameters for HF'
    Cherrypick from master 2011-05-05 19:06:37 UTC franzoni 'add one (I am exhausted)':
        ECALTime/EcalTimePi0/python/ecalTimePi0MakeTreeFromRaw_cfg.py
        ECALTime/EcalTimePi0/python/ecalTimePi0MakeTree_cfg.py
        ECALTime/EcalTimePi0/python/ecalTimePi0Tree_cfi.py
        Hgg/ClusteringWithPU/BuildFile.xml
        Hgg/ClusteringWithPU/interface/SCwithPUhistos.h
        Hgg/ClusteringWithPU/interface/Utils.h
        Hgg/ClusteringWithPU/macro/ClusteringWithPU_macro.C
        Hgg/ClusteringWithPU/plugins/BuildFile.xml
        Hgg/ClusteringWithPU/plugins/SCwithPUAnalysis.cc
        Hgg/ClusteringWithPU/python/scwithpuanalysis_PU20_cfg.py
        Hgg/ClusteringWithPU/src/SCwithPUhistos.cc
        Hgg/ClusteringWithPU/src/Utils.cc
        Hgg/ClusteringWithPU/test/h120GGgluonfusion_7TeV_pu20_cfi_py_GEN_FASTSIM_HLT.py
    Cherrypick from master 2010-07-06 11:13:58 UTC scooper '1) Adding available filtering on L1 triggers/technical triggers (untested).':
        ECALTime/EcalTimePi0/BuildFile
        ECALTime/EcalTimePi0/bin/BuildFile
        ECALTime/EcalTimePi0/bin/readEcalTimePi0Tree.cpp
        ECALTime/EcalTimePi0/interface/EcalTimePi0TreeContent.h
        ECALTime/EcalTimePi0/plugins/BuildFile
        ECALTime/EcalTimePi0/plugins/EcalTimePi0Tree.cc
        ECALTime/EcalTimePi0/plugins/EcalTimePi0Tree.h
        ECALTime/EcalTimePi0/plugins/plugins.cc
        ECALTime/EcalTimePi0/src/EcalTimePi0TreeContent.cc
    Delete:
        ZShape/Base/BuildFile
        ZShape/Base/interface/EfficiencyStatistics.h
        ZShape/Base/interface/EtaAcceptance.h
        ZShape/Base/interface/ZShapeElectron.h
        ZShape/Base/interface/ZShapeEvent.h
        ZShape/Base/interface/ZShapeStandardCuts.h
        ZShape/Base/interface/ZShapeZDef.h
        ZShape/Base/src/EfficiencyStatistics.cc
        ZShape/Base/src/EtaAcceptance.cc
        ZShape/Base/src/ZShapeElectron.cc
        ZShape/Base/src/ZShapeEvent.cc
        ZShape/Base/src/ZShapeStandardCuts.cc
        ZShape/Base/src/ZShapeZDef.cc
        ZShape/Paper/root/plotAcc.C
        ZShape/Paper/root/plotEffAcc.C
        ZShape/Paper/root/plotEffStat.C
        ZShape/Paper/root/plotFinal.C
        ZShape/Paper/root/tdrstyle.C
        ZShape/Paper/root/zrapidityStandard.C
        ZShape/ZFromData/data/Tight-ECAL-HF.txt
        ZShape/ZFromData/data/Tight-ECAL-Loose-ECAL.txt

---
+++ ClusteringWithPU/interface/SCwithPUhistos.h
@@ -0,0 +1,137 @@
+#ifndef PUHS_INC
+#define PUHS_INC
+
+#include <TH1.h>
+#include <TH2.h>
+#include <TH3.h>
+#include "DataFormats/Math/interface/LorentzVector.h"
+#include "DataFormats/Math/interface/Vector3D.h"
+#include "CommonTools/UtilAlgos/interface/TFileService.h"
+
+#include "DataFormats/EcalDetId/interface/EBDetId.h"
+#include "DataFormats/EcalDetId/interface/EEDetId.h"
+
+#include "DataFormats/EgammaReco/interface/SuperCluster.h"
+#include "DataFormats/EgammaReco/interface/SuperClusterFwd.h"
+#include "DataFormats/EgammaCandidates/interface/Photon.h"
+#include "DataFormats/EgammaCandidates/interface/PhotonFwd.h"
+#include "SimDataFormats/GeneratorProducts/interface/HepMCProduct.h"
+#include "DataFormats/Candidate/interface/Particle.h"
+#include "DataFormats/EcalRecHit/interface/EcalRecHitCollections.h"
+
+// holds histograms for pile up studies
+
+class SCwithPUhistos {
+
+public:
+  void Book(TFileDirectory& tfd);
+  
+  void FillSc(const reco::SuperClusterCollection::const_iterator sc1, HepMC::GenEvent::particle_const_iterator particle1,
+	    const EcalRecHitCollection* ebRecHits, const EcalRecHitCollection* eeRecHits);
+  
+  void FillGamma(const reco::PhotonCollection::const_iterator pho1, HepMC::GenEvent::particle_const_iterator particle1);
+
+  void FillH(const std::vector<reco::Photon> higgsPhotons, const std::vector<reco::Photon> higgsPhotons_trueVtx);
+
+  float etaTransformation(  float EtaParticle , float Zvertex);
+  void setRawEnergy(bool useRawEnergy);
+  
+  SCwithPUhistos();
+  SCwithPUhistos(bool useRawEnergy);
+  
+ private:
+  bool useRawEnergy_;
+  
+  TH1F *h_scet_barl;// single-photon mult
+  TH1F *h_scet_endc;// single-photon mult
+  TH1F *h_EoverEtrue_barl;// single-photon mult
+  TH1F *h_EoverEtrue_endc;// single-photon mult
+  TH1F *h_E5x5overEtrue_barl;// single-photon mult
+  TH1F *h_E5x5overEtrue_endc;// single-photon mult
+  TH1F *h_E5x5R9overEtrue_barl;// single-photon mult
+  TH1F *h_PhoER9overEtrue_barl;// single-photon mult
+  TH1F *h_E5x5notR9overEtrue_barl;// single-photon mult
+  TH1F *h_PhoEnotR9overEtrue_barl;// single-photon mult
+  TH1F *h_PhoEoverEtrue_barl;// single-photon mult
+  TH1F *h_PhoEoverEtrue_endc;// single-photon mult
+  TH1F *h_E5x5R9overEtrue_endc;// single-photon mult
+  TH1F *h_PhoER9overEtrue_endc;// single-photon mult
+  TH1F *h_E5x5notR9overEtrue_endc;// single-photon mult
+  TH1F *h_PhoEnotR9overEtrue_endc;// single-photon mult
+
+  TH1F *h_mHiggs_EBEB; // diphoton
+  TH1F *h_mHiggs_EBEE; // diphoton
+  TH1F *h_mHiggs_EEEE; // diphoton
+  TH1F *h_mHiggs_EBEB_trueVtx; // diphoton
+  TH1F *h_mHiggs_EBEE_trueVtx; // diphoton
+  TH1F *h_mHiggs_EEEE_trueVtx; // diphoton
+  TH2F *h_E5x5overEtrueVsEphoEtrue_barl;// single-photon mult
+  TH2F *h_E5x5overEtrueVsEphoEtrue_endc;// single-photon mult
+  TH1F *h_phiWidth;// single-photon mult
+  TH2F *h_phiWidthVsE;// single-photon mult
+  TH1F *h_phiWidth_endc;// single-photon mult
+  TH2F *h_phiWidthVsE_endc;// single-photon mult
+  TH1F *h_phiSize;// single-photon mult
+  TH2F *h_phiSizeVsE;// single-photon mult
+  TH2F *h_phiSizeVsEt;// single-photon mult
+  TH1F *h_phiSize_endc;// single-photon mult
+  TH2F *h_phiSizeVsE_endc;// single-photon mult
+  TH2F *h_phiSizeVsEt_endc;// single-photon mult
+  TH1F *h_phiShape_barl; // single-photon mult
+  TH1F *h_absPhiShape_barl; // single-photon mult
+  TH1F *h_phiShape_endc; // single-photon mult
+  TH1F *h_absPhiShape_endc; // single-photon mult
+  TH2F *h_phiShapeVsE_barl; // single-photon mult
+  TH2F *h_phiShapeVsEt_barl; // single-photon mult
+  TH2F *h_absPhiShapeVsE_barl; // single-photon mult
+  TH2F *h_phiShapeVsE_endc; // single-photon mult
+  TH2F *h_phiShapeVsEt_endc; // single-photon mult
+  TH2F *h_absPhiShapeVsE_endc; // single-photon mult
+  TH1F *h_etaShape_barl;// single-photon mult
+  TH1F *h_etaShape_barlPLus;// single-photon mult
+  TH1F *h_etaShape_barlMinus;// single-photon mult
+  TH1F *h_etaShape_barlSymm;// single-photon mult
+  TH2F *h_etaPhiShape_barl;// single-photon mult
+  TH2F *h_etaPhiShape_barlPLus;// single-photon mult
+  TH2F *h_etaPhiShape_barlMinus;// single-photon mult
+  TH2F *h_etaPhiShape_barlSymm;// single-photon mult
+  TH1F *h_phiBCminusPhiSeed_barl;// single-photon mult
+  TH1F *h_etaBCminusEtaSeed_barl;// single-photon mult
+  TH1F *h_absEtaBCminusAbsEtaSeed_barl;// single-photon mult
+  TH1F *h_absEaBCminusAbsEtaSeed_barl;// single-photon mult
+  TH2F *h_phiBCminusPhiSeed_VS_etaBCminusEtaSeed_barl;// single-photon mult
+  TH2F *h_phiBCminusPhiSeed_VS_etaBCminusEtaSeed_barlm1;// single-photon mult
+  TH1F *h_phiBCminusPhiSeed_barlm1;// single-photon mult
+  TH1F *h_etaBCminusEtaSeed_barlm1;// single-photon mult
+  TH1F *h_absEtaBCminusAbsEtaSeed_barlm1;// single-photon mult
+  TH2F *h_phiBCminusPhiSeed_VS_etaBCminusEtaSeed_barlm2;// single-photon mult
+  TH1F *h_phiBCminusPhiSeed_barlm2;// single-photon mult
+  TH1F *h_etaBCminusEtaSeed_barlm2;// single-photon mult
+  TH1F *h_absEtaBCminusAbsEtaSeed_barlm2;// single-photon mult
+  TH2F *h_phiBCminusPhiSeed_VS_etaBCminusEtaSeed_barlm3;// single-photon mult
+  TH1F *h_phiBCminusPhiSeed_barlm3;// single-photon mult
+  TH1F *h_etaBCminusEtaSeed_barlm3;// single-photon mult
+  TH1F *h_absEtaBCminusAbsEtaSeed_barlm3;// single-photon mult
+  TH2F *h_phiBCminusPhiSeed_VS_etaBCminusEtaSeed_barlm4;// single-photon mult
+  TH1F *h_phiBCminusPhiSeed_barlm4;// single-photon mult
+  TH1F *h_etaBCminusEtaSeed_barlm4;// single-photon mult
+  TH1F *h_absEtaBCminusAbsEtaSeed_barlm4;// single-photon mult
+  TH1F *h_maxCryInDomino_barl;// single-photon mult
+  TH2F *h_maxCryInDominoVsPhi_barl;// single-photon mult
+  TH1F *h_maxCryInLocMax_barlPLus;// single-photon mult
+  TH2F *h_maxCryInLocMaxVsPhi_barlPLus;// single-photon mult
+  TH1F *h_maxCryInLocMax_barlMinus;// single-photon mult
+  TH2F *h_maxCryInLocMaxVsPhi_barlMinus;// single-photon mult
+  TH1F *h_maxCryInLocMax_barlSymm;// single-photon mult
+  TH2F *h_maxCryInLocMaxVsPhi_barlSymm;// single-photon mult
+
+  TH1F *h_numBC;
+  TH2F *h_EoverEtrue_VS_phiWidth_barl;
+  TH2F *h_EoverEtrue_VS_phiSize_barl;
+  TH2F *h_EoverEtrue_VS_DeltaPhi_TwoCl_barl;
+  TH2F *h_EoverEtrue_VS_DeltaPhibcminObcMax_barl;
+  TH2F *h_EoverEtrue_VS_bcminObcMax_barl;
+
+}; 
+
+#endif
